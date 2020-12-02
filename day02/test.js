4a762331214754a01e00a35cf83a1989d698103c
Brian Sinkovec <brian.sinkovec@rwth-aachen.de>
2020-12-02 11:20:17 +0100

    minor

--- day02/test.js
+++ day02/test.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 const one = require('./one');
 const two = require('./two');
 
-describe('Day 1: Report Repair', () => {
+describe('Day 2: Password Philosophy', () => {
     let input = 
 `1-3 a: abcde
 1-3 b: cdefg
