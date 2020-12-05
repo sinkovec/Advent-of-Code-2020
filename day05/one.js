7cbc8af9e7b348208198a933e1e232d766e1955f
Brian Sinkovec <brian.sinkovec@rwth-aachen.de>
2020-12-05 17:26:59 +0100

    day 5

--- day05/one.js
+++ day05/one.js
@@ -1,23 +1,3 @@
 module.exports = (input) => {
-    const data = input.split('\n').map(e => {
-        var rowSearchString = e.substring(0,7);
-        var columnSearchString = e.substring(7);
-        return binarySearch(rowSearchString, 'F', 'B') * 8 + binarySearch(columnSearchString, 'L', 'R');
-    });
-    return Math.max(...data);
+    return Math.max(...input.split('\n').map(e => parseInt(e.replace(/F|L/g,0).replace(/B|R/g,1),2)));
 };
-
-function binarySearch(searchString, lowChar, highChar) {
-    var low = 0;
-    var high = Math.pow(2, searchString.length) - 1;
-    var mid = Math.floor(high / 2);
-    for (const c of searchString) {
-        if (c === lowChar) {
-            high = mid;
-        } else if (c === highChar) {
-            low = mid + 1;
-        }
-        mid = low + Math.floor((high - low) / 2);
-    }
-    return mid;
-}
--- day05/two.js
+++ day05/two.js
@@ -1,29 +1,11 @@
 module.exports = (input) => {
-    const data = input.split('\n').map(e => {
-        var rowSearchString = e.substring(0,7);
-        var columnSearchString = e.substring(7);
-        return binarySearch(rowSearchString, 'F', 'B') * 8 + binarySearch(columnSearchString, 'L', 'R');
-    });
-    data.sort((a,b) => a - b);
+    const seatIDs = input.split('\n')
+        .map(e => parseInt(e.replace(/F|L/g,0).replace(/B|R/g,1),2))
+        .sort((a,b) => a - b);
 
-    for (var i = 1; i < data.length - 1; i++) {
-        if (data[i]+2 == data[i+1]) {
-            return data[i]+1;
+    for (var i = 1; i < seatIDs.length - 1; i++) {
+        if (seatIDs[i]+2 == seatIDs[i+1]) {
+            return seatIDs[i]+1;
         }
     }
 };
-
-function binarySearch(searchString, lowChar, highChar) {
-    var low = 0;
-    var high = Math.pow(2, searchString.length) - 1;
-    var mid = Math.floor(high / 2);
-    for (const c of searchString) {
-        if (c === lowChar) {
-            high = mid;
-        } else if (c === highChar) {
-            low = mid + 1;
-        }
-        mid = low + Math.floor((high - low) / 2);
-    }
-    return mid;
-}
