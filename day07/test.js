6d83077a29b0c36d25225b60fbb8a8ac93095b62
Brian Sinkovec <brian.sinkovec@rwth-aachen.de>
2020-12-07 11:08:58 +0100

    day 7; part 2

--- day07/test.js
+++ day07/test.js
@@ -14,12 +14,25 @@ vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 faded blue bags contain no other bags.
 dotted black bags contain no other bags.`;
 
+let input2 =
+`shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.`;
+
 describe('Day 7: Handy Haversacks', () => {
     it('Part One', () => {
         assert.strictEqual(one(input), 4);
     });
 
-    it('Part Two', () => {
-        assert.strictEqual(two(input), 0);
+    it('Part Two: test input', () => {
+        assert.strictEqual(two(input), 32);
+    });
+
+    it('Part Two: test input2', () => {
+        assert.strictEqual(two(input2), 126);
     });
 });
--- day07/two.js
+++ day07/two.js
@@ -1,3 +1,21 @@
 module.exports = (input) => {
-    return 0;
+    const data = input.split('\n').reduce((map,line) => {
+        var [key, value] = line.substring(0,line.length-1).split(' bags contain ');
+        map[key] = value.split(',').map(s => s.trim()).reduce((map,rule) => {
+            var [num, color] = rule.split(/\s(.+)/);
+            color = color.substring(0, color.lastIndexOf(' bag'));
+            map[color] = num === 'no' ? 0 : Number(num);
+            return map;
+        }, {});
+        return map;
+    }, {});
+    return getNumberOfBags(data, 'shiny gold') - 1;
 };
+
+function getNumberOfBags(data, bag) {
+    var res = 1;
+    for (const subBag in data[bag]) {
+        res += data[bag][subBag] * getNumberOfBags(data,subBag);
+    }
+    return res;
+}
