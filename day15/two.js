b8f81304ba13d136dacc4177ad4a0d00218afec6
Brian Sinkovec <brian.sinkovec@rwth-aachen.de>
2020-12-15 14:18:19 +0100

    remove BigInt

--- day15/two.js
+++ day15/two.js
@@ -1,13 +1,13 @@
 module.exports = (input) => {
-    var startingNumbers = input.split(',').map(BigInt);
-    var numbers = startingNumbers.reduce((map,current,index) => map.set(current,BigInt(index+1)), new Map());
-    var spokenNumber = BigInt(0);
-    var turn = BigInt(startingNumbers.length + 1);
+    var startingNumbers = input.split(',').map(Number);
+    var numbers = startingNumbers.reduce((map,current,index) => map.set(current,index+1), new Map());
+    var spokenNumber = 0;
+    var turn = startingNumbers.length + 1;
     while (turn < 30000000) {
-        var newSpokenNumber = numbers.has(spokenNumber) ? turn - numbers.get(spokenNumber) : BigInt(0);
+        var newSpokenNumber = numbers.has(spokenNumber) ? turn - numbers.get(spokenNumber) : 0;
         numbers.set(spokenNumber, turn);
         spokenNumber = newSpokenNumber;
         turn++;
     }
-    return Number(spokenNumber);
+    return spokenNumber;
 };
